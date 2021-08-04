756be0aa5ba9f03367d97f48d5cd125ae5309be0
peketisivaramganesh <66934109+peketisivaramganesh@users.noreply.github.com>
2021-08-04 20:39:12 +0530

    Second Commit

---
+++ Example.java
@@ -0,0 +1,12 @@
+class Sample
+{
+	public static void main(String [] args)
+	{
+		Sample obj =new Sample();
+		obj.add(40,50);
+	}
+	void add(int a, int b)
+	{
+		System.out.println("addition of two numbers is"+(a+b));
+	}
+}
